d15137ba05ebcf33b4cce5d859e170a7a012559d
Andrew Rapo <andrew@wwlib.org>
2018-10-22 21:39:59 -0400

    update homepage/PUBLIC_URL

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,25 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 
-
 import Application from './components/Application';
 import Model from './model/Model';
+
+const homepage = require('../../package.json').homepage;
 
 declare let module: any
 
 import './css/app.css';
 
 let model: Model = new Model();
+console.log(`window.location.hostname: `, window.location.hostname, window.location.host, homepage);
+switch (window.location.hostname) {
+    case 'localhost':
+        process.env.PUBLIC_URL = '';
+        break;
+    case 'wwlib.org':
+        process.env.PUBLIC_URL = `/${homepage}/`;
+        break;
+}
 console.log(`process.env.PUBLIC_URL: `, process.env.PUBLIC_URL);
 
 // model.on('ready', () => {
